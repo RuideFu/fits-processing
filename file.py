20777429e375ae578ee45ca48416e99db3ad9a1b
Reed Fu <rfugithub@outlook.com>
2022-05-30 18:19:06 -0400

    implement pixel by pixel correction

--- file.py
+++ file.py
@@ -1,13 +1,25 @@
 from astropy.io import fits
+from util import correction
 
-b_image = fits.open('47_tuc_7165651_B_0006_reduced.fits')
-v_image = fits.open('47_tuc_7165651_V_0008_reduced.fits')
-r_image = fits.open('47_tuc_7165651_R_0010_reduced.fits')
+# Read in files
+b_image = fits.open('47_tuc_7165651_R_0010_reduced.fits')
+g_image = fits.open('47_tuc_7165651_V_0008_reduced.fits')
+r_image = fits.open('47_tuc_7165651_B_0006_reduced.fits')
 
-print(b_image.info())
-print(v_image.info())
-print(r_image.info())
+# for r in range(500):
+#     for c in range(500):
+#         r_data[r][c] = 0
+#
+# r_image[0].data = r_data
+# r_image.writeto("R_Output.fits", overwrite=True)
 
-b_image.close()
-v_image.close()
+[output_r, output_g, output_b] = correction(r_image, g_image, b_image)
+
+output_r.writeto("R_Output.fits", overwrite=True)
+output_g.writeto("G_Output.fits", overwrite=True)
+output_b.writeto("B_Output.fits", overwrite=True)
+
+# Close images
 r_image.close()
+g_image.close()
+b_image.close()
---
+++ util.py
@@ -0,0 +1,89 @@
+from math import log10
+
+def filter_wavelength(filter):
+    wavelength = {
+        "U":	0.366,
+        "B":	0.438,
+        "V":	0.545,
+        "R":	0.641,
+        "I":	0.798,
+        "u'":	0.35,
+        "g\'":	0.475,
+        "r\'":	0.6222,
+        "i\'":	0.7362,
+        "z\'":	0.9049,
+        "J":	1.235,
+        "H":	1.662,
+        "Ks":	2.159,
+    }
+    return wavelength[filter]
+
+
+def correction(x_image, y_image, z_image):
+    # Assign headers
+    x_header = x_image[0].header
+    y_header = y_image[0].header
+    z_header = z_image[0].header
+
+    # Identify filters
+    x_filter = x_header['Filter']
+    y_filter = y_header['Filter']
+    z_filter = z_header['Filter']
+
+
+
+    # Lookup wavelength
+    x_wavelength = filter_wavelength(x_filter)
+    y_wavelength = filter_wavelength(y_filter)
+    z_wavelength = filter_wavelength(z_filter)
+
+    # human eye wavelength
+    b_wavelength = 0.465
+    g_wavelength = 0.532
+    r_wavelength = 0.630
+
+    # wavelength ratio
+    lambda_y_x = y_wavelength/x_wavelength
+    lambda_z_y = z_wavelength/y_wavelength
+    lambda_g_b = g_wavelength/b_wavelength
+    lambda_r_g = r_wavelength/g_wavelength
+
+
+    # Zero correction values
+    x_zp = 1
+    y_zp = 0.6534314
+    z_zp = 0.561823628
+
+
+
+    # Extract data
+    x_data = x_image[0].data
+    y_data = y_image[0].data
+    z_data = z_image[0].data
+
+    row_count = x_data.shape[0]
+    col_count = x_data.shape[1]
+
+    for row in range(row_count):
+        for col in range(col_count):
+            x_pixel = x_data[row][col]
+            y_pixel = y_data[row][col]
+            z_pixel = z_data[row][col]
+            if x_pixel >= 0 and y_pixel >= 0 and z_pixel >= 0:
+                x_pixel = \
+                    y_pixel * 10 ** \
+                    ((log10(x_pixel/y_pixel) - (x_zp - y_zp)/2.5)*lambda_g_b/lambda_y_x)
+                z_pixel = \
+                    y_pixel * 10 ** \
+                    ((log10(z_pixel/y_pixel) - (z_zp - y_zp)/2.5)*lambda_r_g/lambda_z_y)
+
+            x_data[row][col] = x_pixel
+            y_data[row][col] = y_pixel
+            z_data[row][col] = z_pixel
+
+    # Apply new data
+    x_image[0].data = x_data
+    y_image[0].data = y_data
+    z_image[0].data = z_data
+
+    return [z_image, y_image, x_image]
