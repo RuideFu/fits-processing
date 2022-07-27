32b9f859e5ede0f308b01c6129e0448fe6b703ff
loggy <loggy@email.unc.edu>
2022-07-27 18:13:05 -0400

    pixel eradicator V1

--- pixelEradicator.py
+++ pixelEradicator.py
@@ -1,27 +1,51 @@
-from numpy import sort, median
+from math import erf
+
+from numpy import median, floor, sqrt
 
 
 def pixel_eradicator(M, image):
     # read in the images and get their data
     data = image[0].data
-
     row_count = data.shape[0]
     col_count = data.shape[1]
     # move through each pixel
     for row in range(row_count):
         for col in range(col_count):
-            pixel = data[row][col]
+            pixel = [data[row][col]]
             # define the M pixels to its left and right
-            pixels_left = []
-            pixels_right = []
-            for i in range(1, M):
-                pixels_left[i] = [data[row-i][col]]
-                pixels_right[i] = [data[row+i][col]]
+            pixel_set = []
+            for j in range(1, M + 1):
+                try:
+                    pixel_set.append(data[row][col - j])
+                except:
+                    continue
+                try:
+                    pixel_set.append(data[row][col + j])
+                except:
+                    continue
 
-            # find the median
-            pixel_set = (pixel, pixels_left, pixels_right)
+            # populate sets with the middle pixel and find the median
+            pixel_set.extend(pixel)
             pixel_median = median(pixel_set)
+            print(pixel_set, pixel_median)
             # calculate sigma
-            # N = 2*M + 1
-            # for j in range(N):
-            print(pixel_set), print(pixel_median)
+            N = len(pixel_set)
+            correction = 1 + (1.7 / N)
+            i = floor(0.683 * N)
+            i_minus = 0.683 * (N - 1)
+            absdev = abs(pixel_median - i)
+            absdev_plus = abs(pixel_median - (i + 1))
+
+            sigma = (absdev + ((absdev_plus - absdev) * (i_minus - floor(i_minus)))) * correction
+
+            rejection_factor = sigma * sqrt(2) * erf(1 - 0.5/N) ** -1
+
+            if abs(pixel_median - pixel) > rejection_factor:
+                data[row][col] = 0
+            else:
+                data[row][col] = 1
+
+    # Apply new data
+    image[0].data = data
+
+    return [image]
--- pixelEradicatorGenerator.py
+++ pixelEradicatorGenerator.py
@@ -2,9 +2,17 @@ from astropy.io import fits
 from pixelEradicator import pixel_eradicator
 # Read in files
 image = fits.open('brokenColumnP6.fits')
-# eradicate the bad pixels, M = 1
-output = pixel_eradicator(2, image)
+# eradicate the bad pixels, M = 1, 2, 3, 4, 5
+output1 = pixel_eradicator(1, image)
+output2 = pixel_eradicator(2, image)
+output3 = pixel_eradicator(3, image)
+output4 = pixel_eradicator(4, image)
+output5 = pixel_eradicator(5, image)
 # overwrite the image
-output.writeto("eradicated.fits", overwrite=True)
+output1.writeto("eradicatedM1.fits", overwrite=True)
+output2.writeto("eradicatedM2.fits", overwrite=True)
+output3.writeto("eradicatedM3.fits", overwrite=True)
+output4.writeto("eradicatedM4.fits", overwrite=True)
+output5.writeto("eradicatedM5.fits", overwrite=True)
 # close the image
 image.close()
